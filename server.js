7c7d2d4542bca82680a6b5e23436658e80f3a125
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-06 09:47:01 -0700

    add Heroku

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const { animals } = require('./data/animals.json');
 const app = express();
 
 function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
     let filteredResults = animalsArray;
+    if(query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
     if(query.diet) {
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
