e7eb59a5e5f8bcfa6eb7b65cb9cae8a8bf2d4800
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-06 10:07:21 -0700

    updated hardcoded PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { animals } = require('./data/animals.json');
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -39,5 +39,5 @@ app.get('/api/animals', (req, res) => {
 });
 
 app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
+    console.log(`API server now on port ${PORT}!`);
 });
