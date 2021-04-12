1290689c4b6bf00505f611cdaa7505970a55027e
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-04-12 11:35:12 -0700

    add zookeeper routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const animalRoutes = require('./animalRoutes');
+// const zookeeperRoutes = require('./zookeeperRoutes');
 
 router.use(animalRoutes);
+router.use(require('./zookeeperRoutes'));
 
 module.exports = router;
